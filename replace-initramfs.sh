41d16a03e76014c7efe65810df3919a8ccabdc35
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2022-07-29 16:19:42 +0100

    replace-initramfs: also inject snapd in rootfs

--- replace-initramfs.sh
+++ replace-initramfs.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -exu
 
-replace_snapd_binaries() {
+BINPATH=~/go/src/github.com/snapcore/snapd
+
+replace_initramfs_bits() {
     KERNEL_EFI_ORIG=cache/snap-pc-kernel/kernel.efi
     if [ ! -d initrd ]; then
         objcopy -O binary -j .initrd "$KERNEL_EFI_ORIG" initrd.img
@@ -18,7 +20,6 @@ replace_snapd_binaries() {
             tar xf - ./usr/lib/ubuntu-core-initramfs/efi/linuxx64.efi.stub
     fi
 
-    BINPATH=~/go/src/github.com/snapcore/snapd
     cp "$BINPATH"/snap-bootstrap initrd/main/usr/lib/snapd/
     cd initrd/main
     find . | cpio --create --quiet --format=newc --owner=0:0 | lz4 -l -7 > ../../initrd.img.new
@@ -44,21 +45,27 @@ cleanup() {
 main() {
     MNT=mnt-replace
     IMG=boot.img
-    mkdir -p "$MNT"/ubuntu-boot
+    mkdir -p "$MNT"/ubuntu-boot "$MNT"/data
 
-    replace_snapd_binaries
+    replace_initramfs_bits
 
     # shellcheck disable=SC2064
     trap "cleanup ./$IMG ./$MNT" EXIT
 
     sudo kpartx -av "$IMG"
     loop=$(sudo kpartx -l "$IMG" |tr -d " " | cut -f1 -d:|sed 's/..$//'|head -1)
-    loop_boot="${loop}"p3
+    loop_boot="$loop"p3
     sudo mount /dev/mapper/"$loop_boot" "$MNT"/ubuntu-boot
 
-    # XXX
     subpath=$(readlink "$MNT"/ubuntu-boot/EFI/ubuntu/kernel.efi)
     cp -a kernel.efi "$MNT"/ubuntu-boot/EFI/ubuntu/"$subpath"
+
+    # replace snapd in data partition
+    data_mnt="$loop"p5
+    sudo mount /dev/mapper/"$data_mnt" "$MNT"/data
+    sudo cp "$BINPATH"/snapd "$MNT"/data/usr/lib/snapd/
+    sudo sed 's/#Nice=-5/Environment=SNAPD_DEBUG=1/' \
+         "$MNT"/data/usr/lib/systemd/system/snapd.service
 }
 
 main
