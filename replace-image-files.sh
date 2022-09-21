cc6e8e0491a54fb0aac68e41f1a9d916f787e81a
Alfonso Sánchez-Beato <alfonso.sanchez-beato@canonical.com>
2022-09-21 16:40:37 +0100

    Sync with snapd PR replace script

--- replace-image-files.sh
+++ replace-image-files.sh
@@ -1,9 +1,7 @@
 #!/bin/bash -exu
 
-BINPATH=~/go/src/github.com/snapcore/snapd
-
 replace_initramfs_bits() {
-    KERNEL_EFI_ORIG=cache/snap-pc-kernel/kernel.efi
+    KERNEL_EFI_ORIG="$CACHE_DIR"/snap-pc-kernel/kernel.efi
     if [ ! -d initrd ]; then
         objcopy -O binary -j .initrd "$KERNEL_EFI_ORIG" initrd.img
         unmkinitramfs initrd.img initrd
@@ -13,23 +11,26 @@ replace_initramfs_bits() {
         cp replace-files/* "$SYSTEMD_D"/
     fi
 
-    uc_initramfs_deb=ubuntu-core-initramfs_55_amd64.deb
-    if [ ! -f "$uc_initramfs_deb" ]; then
-        wget -q https://launchpad.net/~snappy-dev/+archive/ubuntu/image/+files/"$uc_initramfs_deb"
-        dpkg --fsys-tarfile "$uc_initramfs_deb" |
-            tar xf - ./usr/lib/ubuntu-core-initramfs/efi/linuxx64.efi.stub
-    fi
+    # Retrieve efi stub from ppa so we can rebuild kernel.efi
+    sudo DEBIAN_FRONTEND=noninteractive apt install -y --no-install-recommends ubuntu-dev-tools
+    codename=$(lsb_release -cs)
+    arch=$(dpkg-architecture -q DEB_BUILD_ARCH)
+    pull-lp-debs -a "$arch" -D ppa \
+                 --ppa ppa:snappy-dev/image ubuntu-core-initramfs "$codename"
+    dpkg --fsys-tarfile ubuntu-core-initramfs_*.deb |
+        tar --wildcards -xf - './usr/lib/ubuntu-core-initramfs/efi/linux*.efi.stub'
 
-    cp "$BINPATH"/snap-bootstrap initrd/main/usr/lib/snapd/
+    cp "$SNAPD_BINPATH"/snap-bootstrap initrd/main/usr/lib/snapd/
     cd initrd/main
     find . | cpio --create --quiet --format=newc --owner=0:0 | lz4 -l -7 > ../../initrd.img.new
     cd -
 
     objcopy -O binary -j .linux "$KERNEL_EFI_ORIG" linux
+    # Replace kernel.efi in unsquashed snap
     objcopy --add-section .linux=linux --change-section-vma .linux=0x2000000 \
             --add-section .initrd=initrd.img.new --change-section-vma .initrd=0x3000000 \
-            usr/lib/ubuntu-core-initramfs/efi/linuxx64.efi.stub \
-            kernel.efi
+            usr/lib/ubuntu-core-initramfs/efi/linux*.efi.stub \
+            "$KERNEL_EFI_ORIG"
 }
 
 cleanup() {
@@ -45,30 +46,39 @@ cleanup() {
 
 main() {
     MNT=mnt-replace
-    IMG=boot.img
+
     mkdir -p "$MNT"/ubuntu-boot "$MNT"/data
 
     replace_initramfs_bits
 
     # shellcheck disable=SC2064
-    trap "cleanup ./$IMG ./$MNT" EXIT
+    trap "cleanup \"$IMG\" \"$MNT\"" EXIT
 
     loop=$(sudo kpartx -asv "$IMG" | head -n1 | cut -d' ' -f3)
     loop=${loop%p*}
     loop_boot="$loop"p3
     sudo mount /dev/mapper/"$loop_boot" "$MNT"/ubuntu-boot
 
+    # copy kernel.efi with modified initramfs
     subpath=$(readlink "$MNT"/ubuntu-boot/EFI/ubuntu/kernel.efi)
-    cp -a kernel.efi "$MNT"/ubuntu-boot/EFI/ubuntu/"$subpath"
+    cp -a "$CACHE_DIR"/snap-pc-kernel/kernel.efi "$MNT"/ubuntu-boot/EFI/ubuntu/"$subpath"
 
-    # replace snapd in data partition
+    # replace snapd in data partition with the one compiled in the test
     data_mnt="$loop"p5
     sudo mount /dev/mapper/"$data_mnt" "$MNT"/data
-    sudo cp "$BINPATH"/snapd "$MNT"/data/usr/lib/snapd/
-    sudo sed 's/#Nice=-5/Environment=SNAPD_DEBUG=1/' \
-         "$MNT"/data/usr/lib/systemd/system/snapd.service
-    # XXX service file seems to be restored, use brute force for the moment
-    printf 'SNAPD_DEBUG=1\n' | sudo tee -a "$MNT"/data/etc/environment
+    #sudo cp ../../../../../snapd_*.deb "$MNT"/data/snapd.deb
+    #sudo chroot "$MNT"/data apt install -y --no-install-recommends ./snapd.deb
+    #sudo rm "$MNT"/data/snapd.deb
+    sudo cp "$SNAPD_BINPATH"/snapd "$MNT"/data/usr/lib/snapd/
+    # enable debug traces
+    sudo mkdir -p "$MNT"/data/etc/systemd/system/snapd.service.d/
+    sudo tee "$MNT"/data/etc/systemd/system/snapd.service.d/override.conf <<'EOF'
+[Service]
+Environment=SNAPD_DEBUG=1
+EOF
 }
 
+IMG="${1:-./boot.img}"
+CACHE_DIR="${2:-./cache}"
+SNAPD_BINPATH="${3:-$HOME/go/src/github.com/snapcore/snapd}"
 main
